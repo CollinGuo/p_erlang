996f22ed0ba0949dcab47d19065f767a3405151d
CollinGuo <shuieryin@gmail.com>
2015-04-11 19:41:21 +0800

    some minor changes on afile_server.erl

--- src/afile_server.erl
+++ src/afile_server.erl
@@ -5,7 +5,7 @@
 -module(afile_server).
 
 %% ====================================================================
-%% API functions
+%% API function
 %% ====================================================================
 -export([start/1, loop/1]).
 
@@ -22,29 +22,15 @@ loop(Dir) ->
         {Client, list_dir} ->
             Client ! {self(), file:list_dir(Dir)};
         {Client, {get_file, File}} ->
-            Client ! {self(), getFile(Dir, File)};
+            Client ! {self(), file:read_file(filename:join(Dir, File))};
         {Client, {put_file, FromFile, ToFileName}} ->
             FullToFile = filename:join(Dir, ToFileName),
-%%             FileTp = file:read_file(FromFile),
-%%             FileStatus = element(1, FileTp),
-%%             Result = element(2, file:read_file(FromFile)),
-%%             if
-%%                 ok == FileStatus ->
-%%                     Client ! {self(), file:write_file(FullToFile, Result)};
-%%                 error == FileStatus ->
-%%                     Client ! {self(), Result}
-%%             end
-
-            Result = file:read_file(FromFile),
-            case Result of
+            {Status, FileBytes} = file:read_file(FromFile),
+            case Status of
                 ok ->
-                    Client ! {self(), file:write_file(FullToFile, Result)};
+                    Client ! {self(), file:write_file(FullToFile, FileBytes)};
                 error ->
-                    Client ! {self(), Result}
+                    Client ! {self(), Status}
             end
     end,
     loop(Dir).
-
-getFile(Dir, File) ->
-    Full = filename:join(Dir, File),
-    file:read_file(Full).
