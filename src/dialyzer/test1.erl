b2e2221514d8696f0f4580a3ae4469042cb0705d
shuieryin <shuieryin@gmail.com>
2015-04-28 20:52:21 +0800

    added chapter 9 learnings

---
+++ src/dialyzer/test1.erl
@@ -0,0 +1,20 @@
+%% API
+-module(test1).
+-export([f1/0]).
+
+%%%-------------------------------------------------------------------
+%%% @author Li
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 28. Apr 2015 7:22 PM
+%%%-------------------------------------------------------------------
+-author("Li").
+
+f1() ->
+	X = erlang:time(),
+	seconds(X).
+
+seconds({_Year, _Month, _Day, Hour, Min, Sec}) ->
+	(Hour * 60 + Min) * 60 + Sec.
