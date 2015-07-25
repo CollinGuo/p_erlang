6fcd3b7d0eebac7718d37f4ccee188188675e820
CollinGuo <shuieryin@gmail.com>
2015-07-25 22:08:34 +0800

    integrate with webstorm

--- src/websockets/clock1.erl
+++ src/websockets/clock1.erl
@@ -1,41 +1,30 @@
-%% API
 -module(clock1).
 -export([start/1, current_time/0]).
--import(io_lib, [format/2]).
-
-%%%-------------------------------------------------------------------
-%%% @author Li
-%%% @copyright (C) 2015, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 16. Jul 2015 9:28 PM
-%%%-------------------------------------------------------------------
--author("Li").
+%%% NOTE: lines with three %%% show code when frames are introduced
 
 start(Browser) ->
-	send_time(Browser),
-	running(Browser).
+    Browser ! [{cmd, fill_div}, {id, clock}, {txt, current_time()}],
+    running(Browser).
 
 running(Browser) ->
-	receive
-		{Browser, {struct, [{clicked, <<"stop">>}]}} ->
-			idle(Browser)
-	after
-		1000 ->
-			send_time(Browser),
-			running(Browser)
-	end.
-
-send_time(Browser) ->
-	Browser ! [{cmd, fill_div}, {id, clock}, {txt, current_time()}].
+    receive
+        {Browser, {struct, [{clicked, <<"stop">>}]}} ->
+            Browser ! [{cmd, fill_div}, {id, clock}, {txt, <<"Stopped">>}],
+            idle(Browser)
+    after 1000 ->
+        Browser ! [{cmd, fill_div}, {id, clock}, {txt, current_time()}],
+        running(Browser)
+    end.
 
 idle(Browser) ->
-	receive
-		{Browser, {struct, [{clicked, <<"start">>}]}} ->
-			running(Browser)
-	end.
+    receive
+        {Browser, {struct, [{clicked, <<"start">>}]}} ->
+            Browser ! [{cmd, fill_div}, {id, clock}, {txt, <<"Starting">>}],
+            running(Browser)
+    end.
 
 current_time() ->
-	{Hour, Min, Sec} = time(),
-	list_to_binary(format("~2.2.0w:~2.2.0w:~2.2.0w", [Hour, Min, Sec])).
+    {Hour, Min, Sec} = time(),
+    list_to_binary(io_lib:format("~2.2.0w:~2.2.0w:~2.2.0w",
+        [Hour, Min, Sec])).
+
