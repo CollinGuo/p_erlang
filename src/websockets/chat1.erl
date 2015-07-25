f8729229d5243894c21d1765337ea9755d738640
CollinGuo <shuieryin@gmail.com>
2015-07-25 22:33:04 +0800

    check in chat1

---
+++ src/websockets/chat1.erl
@@ -0,0 +1,31 @@
+%% API
+-module(chat1).
+-export([start/1]).
+
+%%%-------------------------------------------------------------------
+%%% @author Li
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25. Jul 2015 10:15 PM
+%%%-------------------------------------------------------------------
+-author("Li").
+
+start(Browser) ->
+	running(Browser, []).
+
+running(Browser, L) ->
+	receive
+		{Browser, {struct, [{join, Who}]}} ->
+			Browser ! [{cmd, append_div}, {id, scroll}, {txt, list_to_binary([Who, " join the group\n"])}],
+			L1 = [Who, "<br>" | L],
+			Browser ! [{cmd, fill_div}, {id, users}, {txt, list_to_binary(L1)}],
+			running(Browser, L1);
+		{Browser, {struct, [{entry, <<"tell">>}, {txt, Txt}]}} ->
+			Browser ! [{cmd, append_div}, {id, scroll}, {txt, list_to_binary([" > ", Txt, "<br>"])}],
+			running(Browser, L);
+		X ->
+			io:format("chat received:~p~n", [X])
+	end,
+	running(Browser, L).
